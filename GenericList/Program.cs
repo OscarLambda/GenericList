45c9fa1fe43c82aa235b4b8b54aa5720eacee8c3
Oscar Lam <oscarlam135@gmail.com>
2019-07-14 16:06:21 -0700

    initial upload

---
+++ GenericList/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace GenericList
+{
+    class Program
+    {
+        //a generic array
+        //add function 
+        //remove function 
+        //contains
+        static void Main(string[] args)
+        {
+            
+        }
+        public class GenericList<T>
+        {
+            int[] intArray = {};
+            public void Add<T>(T[] array)
+            {
+                int[] bigIntArray = new int[intArray.Length + 1];
+            }
+            public void Remove<T>(T[] array)
+            {
+                int[] smallIntArray = new int[intArray.Length - 1];
+            }
+        }
+    }
+}
